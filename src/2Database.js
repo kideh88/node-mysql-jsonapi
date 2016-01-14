3db79b6ed1e8c16656226e738df02586a8fe1a4e
kideh88 <kim.dehmlow@gmail.com>
2016-01-15 00:11:43 +0700

    [BUGFIX] Name conflicts for Database.js

---
+++ src/2Database.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let RequestHandler = require('./RequestHandler');
+let ResponseHandler = require('./ResponseHandler');
+let MySqlDatabase = require('./Database');
+
+class DataHook {
+
+  constructor(config) {
+    this.connection = new MySqlDatabase(config);
+    this.requestHandler = new RequestHandler(this.connection);
+    this.responseHandler = new ResponseHandler();
+  }
+
+}
+
+module.exports = DataHook;
--- src/RequestHandler.js
+++ src/RequestHandler.js
@@ -19,21 +19,3 @@ class RequestHandler {
 }
 
 module.exports = RequestHandler;
-
-'use strict';
-
-let RequestHandler = require('./RequestHandler');
-let ResponseHandler = require('./ResponseHandler');
-let MySqlDatabase = require('./Database');
-
-class DataHook {
-
-    constructor(config) {
-        this.connection = new MySqlDatabase(config);
-        this.requestHandler = new RequestHandler(this.connection);
-        this.responseHandler = new ResponseHandler();
-    }
-
-}
-
-module.exports = DataHook;
